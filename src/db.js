825c860e52c0dbbd6e4c4c2c88badfca49e7d7b5
mateo lopez <62899146+mateo132l@users.noreply.github.com>
2023-06-22 17:16:47 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ import {createPool} from 'mysql2/promise';
 
 
 export const pool = createPool({
-    host: '34.174.49.241',
-    user: 'u0490eq3ygtk7',
-    password: 'Dmhosting123@',
-    port: 3306,
-    database: 'dbc0nuafltzrje'
+    host: '',
+    user: '',
+    password: '@',
+    port: ,
+    database: ''
 })
 
